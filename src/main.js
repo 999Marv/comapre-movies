b197279a7823dd43b152ce8ee38df2a5627e0378
Marvin Siri <marvinsiri123@gmail.com>
2023-10-31 15:42:07 -0400

    first section completed

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
-console.log('ji');
+import movieData from '/src/movie-data.json';
+import './style.css';
+
+console.log(movieData);
+
+//Local Storage Code
+const setLocalStorageKey = (key, value) => {
+  localStorage.setItem(key, JSON.stringify(value));
+};
+
+const getLocalStorageValue = (key) => {
+  try {
+    return JSON.parse(localStorage.getItem(key));
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+};

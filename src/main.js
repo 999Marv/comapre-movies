7d1a86db7b4147da5ad404a86a50e703718f53ab
Marvin Siri <marvinsiri123@gmail.com>
2023-10-31 17:41:07 -0400

    setting up movie list creation

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import movieData from '/src/movie-data.json';
-import './style.css';
 
 console.log(movieData);
 
@@ -16,3 +15,27 @@ const getLocalStorageValue = (key) => {
     return null;
   }
 };
+
+//dom elements
+const form = document.querySelector('#new-movie-form');
+const movieContainer = document.querySelector('#movie-container');
+
+//steps to create movie
+//create li
+// li class = 'movie-card'   aria = "movie information"
+// create h4 tag - movieName
+// create p tag - criticScore
+// create p tag - audienceScore
+// create p tag - domesticGT
+// create p tag - genre
+//append all to li
+//append li to movieContainer
+
+console.log(form);
+console.log(movieContainer);
+
+const main = () => {
+  console.log('yo');
+};
+
+main();
